8f2614e7b79c8477be462f88bba4cb9a64c19f6c
amilcarsal <135087213+amilcarsal@users.noreply.github.com>
2024-11-23 11:58:02 -0600

    exportacion de pool estaba incorrecta

--- my-api/src/lib/db.ts
+++ my-api/src/lib/db.ts
@@ -1,24 +1,11 @@
 import { Pool } from 'pg';
 
-// Configuración de conexión con PostgreSQL
 const pool = new Pool({
-  user: process.env.DB_USER,           // Usuario de la base de datos
-  host: process.env.DB_HOST,           // Host (localhost si está local)
-  database: process.env.DB_NAME,       // Nombre de la base de datos
-  password: process.env.DB_PASSWORD,   // Contraseña
-  port: Number(process.env.DB_PORT),   // Puerto de conexión
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: Number(process.env.DB_PORT),
 });
 
-// Función para ejecutar consultas
-export const query = async (text: string, params?: any[]) => {
-  const client = await pool.connect();
-  try {
-    const result = await client.query(text, params);
-    return result;
-  } catch (err) {
-    console.error('Error ejecutando la consulta:', err);
-    throw err;
-  } finally {
-    client.release();
-  }
-};
+export default pool;
